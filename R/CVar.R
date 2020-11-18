c7eccc32d485118ac6069e7d08d55c6e315ddab7
Qing Qiu <msellaqiu@gmail.com>
2020-11-17 22:41:28 -0800

    update CVar using map_dbl

--- R/CVar.R
+++ R/CVar.R
@@ -1,6 +1,6 @@
 #' calculate the coefficient of variation
 #'
-#' @param x a numeric vector.
+#' @param x a numeric vector or list.
 #' @param remove_na a logical value indicating whether NA values should be contained in the computation. The default value is _FALSE_.
 #' @param ... further arguments passed to or from other methods
 #'
@@ -11,9 +11,5 @@
 #' @examples
 #' CVar(c(1:4))
 CVar <- function(x, remove_na = FALSE, ...){
-  if (!is.numeric(x)) stop("The input values are not numeric vectors\n")
-  if (sum(is.na(x)>0)) warning("The input values contain NA's\n")
-  mean <- mean(x, na.rm = remove_na)
-  sd <- sd(x, na.rm = remove_na)
-  mean/sd
+  purrr::map_dbl(x, ~ mean(.,na.rm = remove_na)/sd(.,na.rm = remove_na) )
 }
