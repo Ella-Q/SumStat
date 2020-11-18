a15e6e4203b3ee65936d0e0a39d4f4f1a4266def
Qing Qiu <msellaqiu@gmail.com>
2020-11-17 22:07:24 -0800

    document CVar

--- R/CVar.R
+++ R/CVar.R
@@ -1,3 +1,15 @@
+#' Coefficient of variation
+#'
+#' @param x a numeric vector.
+#' @param remove_na a logical value indicating whether NA values should be contained in the computation. The default value is _FALSE_.
+#' @param ... further arguments passed to or from other methods
+#'
+#' @return
+#' A vector containing the coefficient of variation of the input data
+#' @export
+#'
+#' @examples
+#' CVar(c(1:4))
 CVar <- function(x, remove_na = FALSE, ...){
   if (!is.numeric(x)) stop("The input values are not numeric vectors\n")
   if (sum(is.na(x)>0)) warning("The input values contain NA's\n")
